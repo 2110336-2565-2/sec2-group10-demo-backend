027d29aecc4cfcfa6491202dd403937b00a2eac5
sern <sern2001@hotmail.com>
2023-02-08 01:25:14 +0700

    Fix path, schema, controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,73 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Response } from 'express';
 
+import { Controller, Param, Post } from '@nestjs/common';
+import { Body, HttpCode, Res } from '@nestjs/common/decorators';
+import {
+  Delete,
+  Get,
+  Put,
+} from '@nestjs/common/decorators/http/request-mapping.decorator';
+import { HttpStatus } from '@nestjs/common/enums';
+
+import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Post('mockCreateUser')
+  @HttpCode(HttpStatus.CREATED)
+  async create(): Promise<boolean> {
+    const mockUser = {
+      username: 'MockMan',
+      email: 'mock_man@mock.com',
+      password: '123456',
+    };
+    await this.usersService.create(mockUser);
+    return true;
+  }
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async getUsers(): Promise<CreateUserDto[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async getUser(@Param() params) {
+    return this.usersService.findById(params.id);
+  }
+
   @Post()
-  async create(@Body() username: string) {
-    return await this.usersService.create(username);
+  @HttpCode(HttpStatus.CREATED)
+  async registerUser(
+    @Body() createUserDto: CreateUserDto,
+    @Res() res: Response,
+  ): Promise<Response> {
+    const user = await this.usersService.create(createUserDto);
+    return res.json({
+      username: user.username,
+      email: user.email,
+      message: 'User created successfully',
+    });
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  async deleteUser(@Param() params) {
+    return this.usersService.delete(params.id);
+  }
+
+  @Put(':id')
+  @HttpCode(HttpStatus.OK)
+  async updateUser(@Param() params, @Body() user: CreateUserDto) {
+    return this.usersService.update(params.id, user);
+  }
+
+  @Get('user/:username')
+  @HttpCode(HttpStatus.OK)
+  async findByUsername(@Param() params) {
+    return this.usersService.findByUsername(params.username);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,16 +1,79 @@
+import * as bcrypt from 'bcrypt';
 import { Model } from 'mongoose';
+import * as mongoose from 'mongoose';
 
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
+import { NotFoundException } from '@nestjs/common/exceptions';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 
-import { User, UserDocument } from './schemas/users.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserDto } from './dto/user.dto';
+import { User, UserDocument } from './schema/users.schema';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+  constructor(
+    @InjectConnection() private readonly connection: mongoose.Connection,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
 
-  async create(username: string): Promise<User> {
-    const createdUser = new this.userModel({ name: username });
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.userModel.findOne({
+      username: createUserDto.username.toLowerCase(),
+    });
+    if (user) {
+      throw new NotFoundException(
+        `User with name ${createUserDto.username} already exists`,
+      );
+    }
+    // hash password
+    const salt = await bcrypt.genSalt();
+    const password = createUserDto.password;
+    createUserDto.password = await bcrypt.hash(password, salt);
+
+    //make email and username insensitivecase
+    createUserDto.email = createUserDto.email.toLowerCase();
+    createUserDto.username = createUserDto.username.toLowerCase();
+
+    const createdUser = new this.userModel(createUserDto);
     return createdUser.save();
+  }
+  async findAll(): Promise<User[]> {
+    return this.userModel.find();
+  }
+
+  async findById(id: string): Promise<UserDto> {
+    const user = await this.userModel.findById(id);
+    if (!user) {
+      throw new NotFoundException(`There isn't any user with id: ${id}`);
+    }
+    const userDto = new UserDto();
+    userDto.username = user.username;
+    userDto.email = user.email;
+    return userDto;
+  }
+
+  async findByUsername(username: string): Promise<UserDto> {
+    return this.userModel.findOne({ username: username.toLowerCase() });
+  }
+
+  async update(id: string, user: UserDto): Promise<UserDto> {
+    try {
+      const userUpdated = await this.userModel.findByIdAndUpdate(id, user, {
+        new: true,
+      });
+      return userUpdated;
+    } catch (err) {
+      throw new NotFoundException(`There isn't any user with id: ${id}`);
+    }
+  }
+
+  async delete(id: string): Promise<UserDto> {
+    try {
+      const userDeleted = await this.userModel.findByIdAndDelete(id);
+      return userDeleted;
+    } catch (err) {
+      throw new NotFoundException(`There isn't any user with id: ${id}`);
+    }
   }
 }
