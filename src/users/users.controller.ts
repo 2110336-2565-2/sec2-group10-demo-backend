615597d545ae44bef9c3e00e615ff7a8faa68972
sern <sern2001@hotmail.com>
2023-02-23 00:30:28 +0700

    Add roleguard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
 import { Response } from 'express';
+import { Role } from 'src/common/enums/role';
+import { Roles } from 'src/roles/roles.decorator';
 
 import { Controller, Param, Post } from '@nestjs/common';
 import { Body, HttpCode, Res } from '@nestjs/common/decorators';
@@ -31,6 +33,7 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles(Role.User)
   @ApiOkResponse({
     description: 'Return users',
     type: [User],
