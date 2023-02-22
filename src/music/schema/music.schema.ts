d44fa486a7a2f52eb51d329024110c81d943eec3
Alones <polbcc167@gmail.com>
2023-02-22 16:05:51 +0700

    feat: music and playlist schema

---
+++ src/music/schema/music.schema.ts
@@ -0,0 +1,109 @@
+import { SchemaTypes, Types } from 'mongoose';
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Schema()
+export class Music {
+  /**
+   * ID of the music
+   * @example 5ff4c9d8e4b0f8b8b8b8b8b8
+   */
+  @ApiProperty({ example: '5ff4c9d8e4b0f8b8b8b8b8b8' })
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    required: true,
+    auto: true,
+  })
+  _id: Types.ObjectId;
+
+  /**
+   * Name of the music
+   * @example My music
+   * @maxLength 50
+   * @minLength 1
+   * @pattern ^[a-zA-Z0-9 ]*$
+   * @required
+   * @type string
+   */
+  @ApiProperty({ example: 'My music' })
+  @Prop({
+    required: true,
+    maxlength: 50,
+    minlength: 1,
+    match: /^[a-zA-Z0-9 ]*$/,
+  })
+  name: string;
+
+  /**
+   * Description of the music
+   * @example My music description
+   * @maxLength 100
+   * @minLength 1
+   * @pattern ^[a-zA-Z0-9 ]*$
+   * @default ''
+   * @type string
+   */
+  @ApiProperty({ example: 'My music description' })
+  @Prop({
+    maxlength: 100,
+    minlength: 1,
+    match: /^[a-zA-Z0-9 ]*$/,
+    default: '',
+  })
+  description: string;
+
+  /**
+   * Date of creation of the music
+   * @example 2021-01-01T00:00:00.000Z
+   * @type string
+   * @format date-time
+   * @default Date.now()
+   */
+  @ApiProperty({ example: '2021-01-01T00:00:00.000Z' })
+  @Prop({
+    default: function () {
+      return Date.now();
+    },
+  })
+  creationDate: Date;
+
+  /**
+   * ID of the album of the music
+   * @example 5ff4c9d8e4b0f8b8b8b8b8b8
+   * @required
+   * @type string
+   */
+  @ApiProperty({ example: '5ff4c9d8e4b0f8b8b8b8b8b8' })
+  @Prop({
+    type: SchemaTypes.ObjectId,
+    required: true,
+  })
+  albumId: Types.ObjectId;
+
+  /**
+   * Image of the music
+   * @example https://www.google.com
+   * @required
+   * @type string
+   */
+  @ApiProperty({ example: 'https://www.google.com' })
+  @Prop({
+    required: true,
+  })
+  image: string;
+
+  /**
+   * URL of the music
+   * @example https://www.google.com
+   * @required
+   * @type string
+   */
+  @ApiProperty({ example: 'https://www.google.com' })
+  @Prop({
+    required: true,
+  })
+  url: string;
+}
+
+export const MusicSchema = SchemaFactory.createForClass(Music);
