e2168185b42e19f828cec2ee566388ea0d072564
tantawit <tantawitp@gmail.com>
2023-03-03 22:20:02 +0700

    Update code from dev

--- src/users/music/musics.service.ts
+++ src/users/music/musics.service.ts
@@ -41,9 +41,12 @@ export class MusicsService {
       throw new BadRequestException('Only image file are allowed');
     }
 
+    // Get music duration
+    const duration = await this.getMusicDuration(music.path);
+
     // Check if Album exist
     try {
-      const album = await this.playlistsService.getPlaylist(
+      const album = await this.playlistsService.getPlaylistInfo(
         new Types.ObjectId(uploadMusicDto.albumId),
       );
     } catch (err) {
@@ -55,10 +58,36 @@ export class MusicsService {
       userId: userId,
       coverImage: coverImage.path,
       url: music.path,
+      duration: duration,
     });
 
     // Create and return the music
     let createdMusic = new this.musicModel(uploadMusicDto);
     return createdMusic.save();
+  }
+
+  async getMusicDuration(url: string) {
+    return '00:02:00';
+    // const stream = fs.createReadStream(
+    //   'https://storage.googleapis.com/demo-tuder-music/Shave%20of%20You.mp3',
+    // );
+    // const duration = await getAudioDurationInSeconds(stream);
+    // return duration;
+
+    // const mp3file =
+    //   'https://raw.githubusercontent.com/prof3ssorSt3v3/media-sample-files/master/doorbell.mp3';
+    // const audioContext = new (window.AudioContext ||
+    //   window.AudioContext)();
+    // const request = new XMLHttpRequest();
+    // request.open('GET', mp3file, true);
+    // request.responseType = 'arraybuffer';
+    // request.onload = function () {
+    //   audioContext.decodeAudioData(request.response, function (buffer) {
+    //     let duration = buffer.duration;
+    //     console.log(duration);
+    //     document.write(duration);
+    //   });
+    // };
+    // request.send();
   }
 }
